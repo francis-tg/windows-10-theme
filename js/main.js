6576216b7481ba9fa0612e11c3e88a233f9a45f5
francis-tg <franc9885@gmail.com>
2023-08-08 15:55:44 +0000

    add start menu bar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function openPan(url) {
-    const pan = createPan(url)
-    pan.classList.add("open")
-    document.querySelector("body").appendChild(pan)
-    const toRms = document.querySelectorAll(".windows-pan")
-    toRms.forEach((toRm) => {
-        toRm.querySelector(".fa-times").addEventListener("click", (e) => {
-            e.preventDefault()
-            toRm.remove()
-        })
-        /* toRm.querySelector(".fa-square").addEventListener("click", (e) => {
+	const pan = createPan(url);
+	pan.classList.add("open");
+	document.querySelector("body").appendChild(pan);
+	const toRms = document.querySelectorAll(".windows-pan");
+	toRms.forEach((toRm) => {
+		toRm.querySelector(".fa-times").addEventListener("click", (e) => {
+			e.preventDefault();
+			toRm.remove();
+		});
+		/* toRm.querySelector(".fa-square").addEventListener("click", (e) => {
             e.preventDefault()
             toRm.setAttribute("full-screen", true);
             toRm.style.left = 0
@@ -26,37 +26,53 @@ function openPan(url) {
             toRm.style.height = "720px"
             toRm.removeAttribute("full-screen");
         }) */
-        
-    })
-    
+	});
 }
 
-function openLink(url){
-    window.open(url, "__blank");
+function openLink(url) {
+	window.open(url, "__blank");
 }
 function createPan(url) {
-    const newPan = document.createElement("div");
-    const createIframe = document.createElement("iframe")
-    newPan.classList.add("windows-pan")
-    const actionBtn = document.createElement("div");
-    actionBtn.classList.add("action-btns")
-    const rmIcon = createIcon("fas","fa-times")
-    const sqreIcon = createIcon("far","fa-square")
-    const minusIcon = createIcon("fas", "fa-minus")
-    actionBtn.appendChild(minusIcon)
-    actionBtn.appendChild(sqreIcon)
-    actionBtn.appendChild(rmIcon)
-    createIframe.src = url
-    newPan.appendChild(actionBtn)
-    createIframe.setAttribute("X-frame-orign","")
-    newPan.appendChild(createIframe)
-    return newPan;
-    
+	const newPan = document.createElement("div");
+	const createIframe = document.createElement("iframe");
+	newPan.classList.add("windows-pan");
+	const actionBtn = document.createElement("div");
+	actionBtn.classList.add("action-btns");
+	const rmIcon = createIcon("fas", "fa-times");
+	const sqreIcon = createIcon("far", "fa-square");
+	const minusIcon = createIcon("fas", "fa-minus");
+	actionBtn.appendChild(minusIcon);
+	actionBtn.appendChild(sqreIcon);
+	actionBtn.appendChild(rmIcon);
+	createIframe.src = url;
+	newPan.appendChild(actionBtn);
+	createIframe.setAttribute("X-frame-orign", "");
+	newPan.appendChild(createIframe);
+	return newPan;
 }
 
-function createIcon(icon_type,custom_class) {
-    const i = document.createElement("i");
-    i.classList.add(icon_type??"fas")
-    i.classList.add(custom_class)
-    return i
+function createIcon(icon_type, custom_class) {
+	const i = document.createElement("i");
+	i.classList.add(icon_type ?? "fas");
+	i.classList.add(custom_class);
+	return i;
 }
+
+document
+	.querySelector("#windows-logo")
+	.parentElement.addEventListener("click", (e) => {
+		e.preventDefault();
+		document.querySelector(".start-menu").classList.toggle("open");
+	});
+document.addEventListener("keyup", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	if (e.key === "Meta") {
+		document.querySelector(".start-menu").classList.toggle("open");
+	}
+});
+
+document.querySelector("[windows-logo]").addEventListener("mouseenter", (e) => {
+	e.target.children[0].style.color = "#03A9F4";
+});
+//document.addEventListener("mouseenter");
